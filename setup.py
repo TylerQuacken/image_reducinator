3f6e55398b9e1f774c01d25fdd290b4f061ad2cb
Tyler Quackenbush <tquackenbush@dwfritz.com>
2023-05-23 10:32:58 -0700

    Package set up

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+   name='image_reducinator',
+   version='0.1',
+   packages=['clusterinator',],
+   license='MIT',
+   package_dir={'': 'src'},
+)
---
+++ src/clusterinator/Clusterinator.py
@@ -0,0 +1,6 @@
+class Clusterinator:
+    """K-Means clusterer"""
+
+    def __init__(self, inputs):
+        """Initializes a Clusterinator to cluster data into groups"""
+        pass
