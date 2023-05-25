622c0c24850427ac3f595f3afb222da0ed7f1107
Tyler Quackenbush <tquackenbush@dwfritz.com>
2023-05-25 11:53:11 -0700

    algorithm executes, but groups swap consistently

--- src/clusterinator/Clusterinator.py
+++ src/clusterinator/Clusterinator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from IPython import embed
 
 class Clusterinator:
@@ -22,14 +23,39 @@ class Clusterinator:
         self.data_dim = self.data.shape[-1]
 
         # I want this to do max/min over all axes except the last one
-        batch_dim_nums = tuple(range(len(data.shape)))
-        self.data_max = np.amax(data, axis=batch_dim_nums)
-        self.data_min = np.amin(data, axis=batch_dim_nums)
+        self.batch_dim_nums = tuple(range(len(data.shape)-1))
+        self.data_max = np.amax(data, axis=self.batch_dim_nums)
+        self.data_min = np.amin(data, axis=self.batch_dim_nums)
         self.data_norm = (self.data - self.data_min)/(self.data_max - self.data_min)
         self.cluster_assigns = np.zeros(self.batch_dims + (self.n_clusters,), dtype=int)
+        # Should change to randomly choose two data points instead of choose two random spots
         self.centroids = np.random.rand(self.n_clusters, self.data_dim)
 
+        self.cluster_deviations = np.ones(n_clusters)
+        # self.cluster_deviations = np.array([1,2])
+
+
+        self.error = None
         self.assign_points_to_clusters()
+
+    def perform_k_means(self, max_iter=10, converge_tol=0.01):
+        converged = False
+        i = 0
+
+        while not(converged) and i < max_iter:
+            
+            self.plot_data_2d()
+
+            self.calculate_centroids()
+            self.assign_points_to_clusters()
+
+            if self.error < converge_tol:
+                converged = True
+            
+            i += 1
+
+
+        return converged
 
     def calculate_centroids(self):
         """Calculates new centroids for each cluster.
@@ -42,25 +68,40 @@ class Clusterinator:
         -------
         none
         """
-        pass
+
+        for i in range(self.n_clusters):
+            cluster_i_points = self.cluster_assigns == i
+            cluster_i_points = np.expand_dims(cluster_i_points, axis=-1)
+            cluster_i_points = np.repeat(cluster_i_points, 2, axis=-1)
+            masked_data = np.ma.masked_array(self.data_norm, mask=cluster_i_points)
+
+
+            self.centroids[i] = np.mean(masked_data, axis=self.batch_dim_nums)
+            
+            
+        
 
     def assign_points_to_clusters(self):
         norm_expand = np.expand_dims(self.data_norm, -2)
         dist_vecs = self.centroids - norm_expand
         dists = np.linalg.norm(dist_vecs, axis=-1)
-        self.cluster_assigns = np.argmin(dists, axis=-1)
+        self.error = np.sum(dists**2)
+        print(self.error)
+        mahalanobis_dists = dists / self.cluster_deviations
+        self.cluster_assigns = np.argmin(mahalanobis_dists, axis=-1)
+
 
     def plot_data_2d(self):
-        import matplotlib.pyplot as plt
-        fig, ax = plt.subplots()
+        plt.clf()
+        ax = plt.gca()
         for i in range(self.n_clusters):
             i_points = self.data[self.cluster_assigns == i]
             ax.scatter(i_points[...,0].flatten(), i_points[...,1].flatten())
 
         
         centroids = self.centroids*(self.data_max - self.data_min) + self.data_min
-        plt.scatter(centroids[...,0], centroids[...,1])
-        plt.show()
+        ax.scatter(centroids[...,0], centroids[...,1])
+        plt.pause(0.3)
 
 
 if __name__ == "__main__":
@@ -71,9 +112,14 @@ if __name__ == "__main__":
     width = 30
     height = 40
     n_channels = 2
-    data = np.random.randint(0, 255, size=(width, height, n_channels))
+    # data = np.random.randint(0, 255, size=(width, height, n_channels))
+
+    data = np.zeros((width, height, n_channels))
+    data[:, :height//2, :] = (np.random.randn(width, height//2, n_channels) + (10*np.random.rand(n_channels) - 1)) * 128
+    data[:, height//2:, :] = (np.random.randn(width, height//2, n_channels) + (10*np.random.rand(n_channels) - 1)) * 128
     # data = np.ones((width, height, n_channels))
     n_clusters = 2
 
     clusterinator = Clusterinator(n_clusters, data)
+    clusterinator.perform_k_means()
     clusterinator.plot_data_2d()
